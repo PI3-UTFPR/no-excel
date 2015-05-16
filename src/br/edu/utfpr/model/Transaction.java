aa476e85109ef807641e70bf99796e14ab53088c
erico dias <ericodias1@gmail.com>
2015-05-16 01:20:16 -0300

    persistindo transaction (models completos)

---
+++ src/br/edu/utfpr/model/Transaction.java
@@ -0,0 +1,63 @@
+package br.edu.utfpr.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Transaction {
+	@Id
+	@GeneratedValue
+	private long id;
+	private Date date;
+	private BigDecimal value;
+	private boolean operation;
+	
+	@ManyToOne
+	private User user;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public boolean isOperation() {
+		return operation;
+	}
+
+	public void setOperation(boolean operation) {
+		this.operation = operation;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}

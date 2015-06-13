1359f63cebbb197ba012e600f78433b4b2d6b0c5
Felipe <felipegomescontato@live.com>
2015-06-12 21:50:44 -0300

    Add customer service

---
+++ src/br/edu/utfpr/model/service/CustomerService.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.model.service;
+
+import br.edu.utfpr.model.Customer;
+import br.edu.utfpr.model.dao.CustomerDAO;
+
+public class CustomerService extends AbstractService<Long, Customer>{
+	
+	public CustomerService(){
+		dao = new CustomerDAO();
+	}
+
+}
